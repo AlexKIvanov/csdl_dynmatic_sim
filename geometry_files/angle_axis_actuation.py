e4d0e7143331cd755553b3ec7cd2453a46424f0d
Alex <alivanov@ucsd.com>
2023-08-16 23:48:38 -0700

    Need to revise thrust vector rotation.

--- geometry_files/angle_axis_actuation.py
+++ geometry_files/angle_axis_actuation.py
@@ -35,6 +35,7 @@ class AngleAxisActuation(csdl.Model):
         self.parameters.declare('vlm_mesh_dict')
         self.parameters.declare('axis_dict')
         self.parameters.declare('actuation_angle_dict')
+        self.parameters.declare('axis_origin_point')
 
     def define(self):
 
@@ -43,5 +44,82 @@ class AngleAxisActuation(csdl.Model):
         vlm_mesh_dict        = self.parameters['vlm_mesh_dict']
         axis_dict            = self.parameters['axis_dict']
         actuation_angle_dict = self.parameters['actuation_angle_dict']
+        axis_origin_point    = self.parameters['axis_origin_point']
+
+
+        # Create the actuation angle input
+        for key, value in actuation_angle_dict.items():
+            temp_act_profile = np.reshape(value, (n,1))
+            actuation_angle = self.create_input(key, val=temp_act_profile)
+
+        # Create normalized axis inputs
+        for key, value in axis_dict.items():
+            axis_name = key
+            x_axis_value = np.reshape(value[:,0], (n,1))                  # Should be a numpy array of shape (n,1)
+            y_axis_value = np.reshape(value[:,1], (n,1))                  # Should be a numpy array of shape (n,1)
+            z_axis_value = np.reshape(value[:,2], (n,1))                  # Should be a numpy array of shape (n,1)
+            
+            x_axis = self.create_input(key+'_x' , val=x_axis_value)
+            y_axis = self.create_input(key+'_y' , val=y_axis_value)
+            z_axis = self.create_input(key+'_z' , val=z_axis_value)
+
+        # Check if the dictionary is empty
+        if len(thrust_vector_dict) == 0:
+            pass
+        else:
+            for key, value in thrust_vector_dict.items():
+                thrust_axis_origin_pt = np.reshape(axis_origin_point, (n,3))
+
+                thrust_origin_name = key + '_origin'
+                thrust_origin_val = value[0]          # Should be a numpy array of shape (n,3)
+
+                thrust_vector_name = key + '_vector'
+                thrust_vector_val = value[1]          # Should be a numpy array of shape (n,3)
+
+                axis_origin_pt = self.create_input('axis_origin_pt', val=thrust_axis_origin_pt)
+
+                thrust_origin = self.create_input(thrust_origin_name, val=thrust_origin_val)
+                thrust_vector = self.create_input(thrust_vector_name, val=thrust_vector_val)
+
+                thrust_origin_translated = thrust_origin - axis_origin_pt
+                thrust_vector_translated = thrust_vector
+
+                quat_origin = self.create_output(thrust_origin_name + '_quat', shape=(n,) + (4,))
+                quat_vector = self.create_output(thrust_vector_name + '_quat', shape=(n,) + (4,))
+
+                quat_origin[:,0] = csdl.cos(actuation_angle / 2)
+                quat_origin[:,1] = csdl.sin(actuation_angle / 2) * x_axis
+                quat_origin[:,2] = csdl.cos(actuation_angle / 2) * y_axis
+                quat_origin[:,3] = csdl.cos(actuation_angle / 2) * z_axis
+                
+                quat_vector[:,0] = csdl.cos(actuation_angle / 2)
+                quat_vector[:,1] = csdl.sin(actuation_angle / 2) * x_axis
+                quat_vector[:,2] = csdl.cos(actuation_angle / 2) * y_axis
+                quat_vector[:,3] = csdl.cos(actuation_angle / 2) * z_axis
+
+                translated_rotated_thrust_origin = csdl.quatrotvec(quat_origin, thrust_origin_translated)
+                translated_rotated_thrust_vector = csdl.quatrotvec(quat_vector, thrust_vector_translated)
+
+                rotated_thrust_origin = translated_rotated_thrust_origin + axis_origin_pt
+                rotated_thrust_vector = translated_rotated_thrust_vector
+
+                self.register_output(thrust_origin_name+'_rotated', rotated_thrust_origin)
+                self.register_output(thrust_vector_name+'_rotated', rotated_thrust_vector)
+
+        # Check if the dictionary is empty
+        if len(vlm_mesh_dict) == 0:
+            pass
+        else:
+            for key, value in vlm_mesh_dict.items():
+                vlm_mesh_name = key
+                vlm_mesh_val = value                 # Should be a numpy array of shape (n,p,3)
+                
+                vlm_mesh = self.create_input(vlm_mesh_name, val=vlm_mesh_val)
 
         
+
+            
+
+
+
+
--- run_scripts/tilt_wing_actuation_outside_ozone.py
+++ run_scripts/tilt_wing_actuation_outside_ozone.py
@@ -1,4 +1,5 @@
 import csdl
+import matplotlib.pyplot as plt
 
 from src.utils.data2csv import create_csv
 from src.caddee.concept.geometry.geometry import Geometry
@@ -6,8 +7,6 @@ from src.caddee.concept.geometry.geometry import Geometry
 # from modopt.scipy_library import SLSQP
 from modopt.snopt_library import SNOPT
 from modopt.csdl_library import CSDLProblem
-
-
 
 # FROM THE CADDEE REPO
 from src.caddee.concept.geometry.geometry import Geometry
@@ -17,9 +16,8 @@ from src.caddee.concept.geometry.geocore.utils.generate_corner_points import gen
 from src.caddee.concept.geometry.geocore.utils.generate_camber_mesh import generate_camber_mesh
 from src.caddee.concept.geometry.geocore.utils.thrust_vector_creation import generate_thrust_vector
 
-
-
 from geometry_files.both_wings_all_nacelles_tiltwing_geometry_points import both_wings_all_nacelles
+from geometry_files.angle_axis_actuation import AngleAxisActuation
 
 import python_csdl_backend
 import numpy as np
@@ -31,6 +29,14 @@ nt = 100
 
 pointsets = both_wings_all_nacelles()
 
+# Acutation Angle Dict
+
+act_dict = dict()
+actuation_angle_start = 0
+actuation_angle_end   = 90
+actuation_array = np.linspace(actuation_angle_start, actuation_angle_end, nt)
+act_dict['actuation_angle'] = actuation_array
+
 # Expand all the geometry pointsets to have a time-axis of size nt 
 expanded_pointsets = dict()
 for key,value in pointsets.items():
@@ -40,5 +46,52 @@ for key,value in pointsets.items():
         temp = np.expand_dims(value, 0)
     expanded_pointsets[key] = np.repeat(temp, nt, 0)
 
+# Defining the thrust vector dictionary
+thrust_vector_dict = dict()
+thrust_vector_dict['front_left_nacelle1'] = (expanded_pointsets['front_left_nacelle1_origin'], expanded_pointsets['front_left_nacelle1_vector'])
+# thrust_vector_dict['front_left_nacelle1_vector'] = expanded_pointsets['front_left_nacelle1_vector']
+thrust_vector_dict['front_left_nacelle2'] = (expanded_pointsets['front_left_nacelle2_origin'], expanded_pointsets['front_left_nacelle2_vector'])
+# thrust_vector_dict['front_left_nacelle2_vector'] = expanded_pointsets['front_left_nacelle2_vector']
+thrust_vector_dict['front_left_nacelle3'] = (expanded_pointsets['front_left_nacelle3_origin'], expanded_pointsets['front_left_nacelle3_vector'])
+# thrust_vector_dict['front_left_nacelle3_vector'] = expanded_pointsets['front_left_nacelle3_vector']
 
-print('hi')
+# Defining an empty vlm dictionary        
+vlm_mesh_dict = dict()
+
+# Defining axis dictionary
+axis_dict = dict()
+axis_dict['front_wing_rotation_axis'] = expanded_pointsets['front_wing_rotation_axis']
+
+axis_origin_front_wing_pt = expanded_pointsets['front_wing_axis_origin']
+
+
+actuation_model = AngleAxisActuation(
+    num_nodes = nt,
+    thrust_vector_dict = thrust_vector_dict,
+    vlm_mesh_dict = vlm_mesh_dict,
+    axis_dict = axis_dict,
+    actuation_angle_dict = act_dict,
+    axis_origin_point = axis_origin_front_wing_pt,
+)
+sim = python_csdl_backend.Simulator(actuation_model, analytics=True)
+sim.run()
+# sim.check_partials(compact_print=True)
+# sim.check_totals(compact_print=False)
+# sim.visualize_implementation()
+
+# PLOTTING THE VECTORS 
+print(sim['front_left_nacelle1_vector_rotated'])
+# Create the plot
+plt.figure(figsize=(10, 6))  # Optional: Set the figure size
+
+# Plot the vector against time
+plt.plot(sim['front_left_nacelle1_vector_rotated'][:,0], label='Vector')
+
+# Add labels and title
+plt.xlabel('Time')
+plt.ylabel('Vector Value')
+plt.title('Time History of a Vector')
+plt.legend()
+
+plt.grid(True)
+plt.show()
